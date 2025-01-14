208d7c0897d63ed29c2695fe93d3532d92b554af
sphealmeon <haolunqiu@gmail.com>
2025-01-14 17:25:10 -0500

    added a single outline

--- src/app/scout/components/changeButton.tsx
+++ src/app/scout/components/changeButton.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 export default function ChangeButton({ name, setMatchState }: {name: string; setMatchState: Function;}){
     return(
-        <Button onClick={() =>
+        <Button variant="outline" color="black" className="absolute bottom-4 right-4" onClick={() =>
             name == "Teleop" ? setMatchState(2) : name == "Auto" ? setMatchState(1) : name == "Match Review" ? setMatchState(3) : setMatchState(0)
         }>
         {name}
