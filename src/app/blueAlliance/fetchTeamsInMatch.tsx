3ab0e4ea8280ca25e8e010af3ddf898051e25f1d
remyyan <remyyan06@crescentschool.org>
2025-01-14 17:31:00 -0500

    Added fetch files

---
+++ src/app/blueAlliance/fetchTeamsInMatch.tsx
@@ -0,0 +1,31 @@
+import { key, teams, useApi } from "../globalVars";
+
+export const FetchTeams = ({ setTeams }: { setTeams: Function }) => {
+    if (useApi) {
+        fetch("https://www.thebluealliance.com/api/v3/event/" + key + "/matches", {
+            method: "GET",
+            headers: {
+              "X-TBA-Auth-Key":
+                "ZsbRGTknrkbJAl3OBXVaRh8loiP9ecki3Ag2q1DpExs7yRg9g0RVsXTY3edbMBQO",
+            },
+          })
+          .then((response) => {
+            if (!response.ok) {
+              throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            return response.json();
+          })
+          .then((data) => {
+            const allTeams: string[] = [];
+            for (let index = 0; index < data.length; index++) {
+              allTeams.push(data[index].team_number + "");
+            }
+            setTeams(allTeams);
+          })
+          .catch((error) => {
+            console.error("Error:", error);
+          });
+    } else {
+        setTeams(teams);
+    }
+};
--- src/app/scout/start/matchselect.tsx
+++ src/app/scout/start/matchselect.tsx
@@ -1,12 +1,29 @@
 import { Checkbox } from "@/components/ui/checkbox";
 import { Input } from "@/components/ui/input";
-
+import { useState } from "react";
 
 export default function MatchSelect(){
+    const [matchNumber, setMatchNumber] = useState("");
+
+    const handleMatchNumberChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const value = event.target.value;
+        setMatchNumber(value);
+
+        if (value) {
+            console.log("input success");
+        }
+    };
+
     return(
         <div className="w-1/2 flex flex-col items-center justify-center">
             <p className="text-2xl mb-6 font-bold">Scouting App</p>
-            <Input className="mb-6" type="text" placeholder="Match Number"/>
+            <Input
+                className="mb-6"
+                type="text"
+                placeholder="Match Number"
+                value={matchNumber}
+                onChange={handleMatchNumberChange}
+            />
             <Input className="mb-6" type="text" placeholder="Team Number"/>
             
             {/* Container for checkbox and label */}
