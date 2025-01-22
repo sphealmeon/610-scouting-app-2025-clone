acc9d23fde93f9a6b93dc62dceaaad520bd83e5f
remyyan <remyyan06@crescentschool.org>
2025-01-22 15:56:08 -0500

    added hovers and enlargened text and buttons

--- src/app/scout/humanplayer/scoring.tsx
+++ src/app/scout/humanplayer/scoring.tsx
@@ -7,36 +7,36 @@ export default function HumanPlayerMain({ setMatchState }: { setMatchState: Func
 
     return (
         <div className="flex flex-col items-center justify-center min-h-screen p-4">
-            <p className="text-2xl mb-6 font-bold text-center">Human Player</p>
+            <p className="text-3xl mb-6 font-bold text-center">Human Player</p>
             <div className="grid grid-cols-2 gap-4 w-full">
                 <div
-                    className="flex items-center justify-center h-80 bg-blue-500 text-white text-center cursor-pointer rounded-lg"
+                    className="flex items-center justify-center h-80 text-3xl font-bold bg-blue-500 hover:bg-blue-400 text-white text-center cursor-pointer rounded-lg"
                     onClick={() => handleClick()}
                 >
                     Blue Scored
                 </div>
                 <div
-                    className="flex items-center justify-center h-80 bg-red-500 text-white text-center cursor-pointer rounded-lg"
+                    className="flex items-center justify-center h-80 text-3xl font-bold bg-red-500 hover:bg-red-400 text-white text-white text-center cursor-pointer rounded-lg"
                     onClick={() => handleClick()}
                 >
                     Red Scored
                 </div>
                 <div
-                    className="flex items-center justify-center h-80 bg-blue-200 text-center cursor-pointer rounded-lg"
+                    className="flex items-center justify-center h-80 text-3xl font-bold bg-blue-400 hover:bg-blue-300 text-white text-center cursor-pointer rounded-lg"
                     onClick={() => handleClick()}
                 >
                     Blue Missed
                 </div>
                 <div
-                    className="flex items-center justify-center h-80 bg-red-200 text-center cursor-pointer rounded-lg"
+                    className="flex items-center justify-center h-80 text-3xl font-bold bg-red-400 hover:bg-red-300 text-white text-center cursor-pointer rounded-lg"
                     onClick={() => handleClick()}
                 >
                     Red Missed
                 </div>
-                <Button className="" onClick={() => setMatchState(0)}>
+                <Button className="h-10" onClick={() => setMatchState(0)}>
                     Back to Start
                 </Button>
-                <Button className="" onClick={() => setMatchState(0)}>
+                <Button className="h-10" onClick={() => setMatchState(0)}>
                     Submit
                 </Button>
             </div>
