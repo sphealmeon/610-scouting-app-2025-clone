4749b9ce8532a4f9ef2760070eae4dcc49874576
Jerry Yao <99371069+Jerry-0215@users.noreply.github.com>
2025-01-25 12:49:43 -0500

    added page.tsx and tableTabs.tsx with shadcn components

---
+++ src/app/stats/tableTabs.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+
+import AutoTable from "./tables/autoTable";
+import { AggregateData } from "@/app/interfaces";
+import ImportantTable from "./tables/importantTable";
+import TeleopTable from "./tables/teleopTable";
+import EndgameTable from "./tables/endgameTable";
+import AllTable from "./tables/allTable";
+
+export default function TableTabs({ teamData }: { teamData: AggregateData[] }) {
+  return (
+    <Tabs defaultValue="importantStats">
+      <TabsList>
+        <TabsTrigger value="importantStats">Important Stats</TabsTrigger>
+        <TabsTrigger value="autoStats">Auto Stats</TabsTrigger>
+        <TabsTrigger value="teleopStats">Teleop Stats</TabsTrigger>
+        <TabsTrigger value="endgameStats">Endgame Stats</TabsTrigger>
+        <TabsTrigger value="allStats">All Stats</TabsTrigger>
+      </TabsList>
+
+      <TabsContent value="importantStats">
+        <ImportantTable teamData={teamData} />
+      </TabsContent>
+      <TabsContent value="autoStats">
+        <AutoTable teamData={teamData} />
+      </TabsContent>
+      <TabsContent value="teleopStats">
+        <TeleopTable teamData={teamData} />
+      </TabsContent>
+      <TabsContent value="endgameStats">
+        <EndgameTable teamData={teamData} />
+      </TabsContent>
+      <TabsContent value="allStats">
+        <AllTable teamData={teamData} />
+      </TabsContent>
+    </Tabs>
+  );
+}
