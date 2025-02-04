5c3686d380aef1c8667e93084fd74c2ed316f9e1
sphealmeon <haolunqiu@gmail.com>
2025-02-04 17:15:51 -0500

    removed disabled button

--- src/app/scout/start/scoutselect.tsx
+++ src/app/scout/start/scoutselect.tsx
@@ -6,15 +6,12 @@ export default function ScoutSelect({setMatchState}: {setMatchState: Function}){
 
     return(
         <div className="flex flex-col justify-center items-center space-y-4">
-            <Button disabled = {matchverify()}
+            <Button 
                 className="mb-20 text-xl py-6 px-8"
                 onClick={() => setMatchState(1)}
             >
                 Robot Scout
             </Button>
-            {/* <Button 
-                className="mb-20 text-xl py-6 px-8"
-            >Robot Scout</Button> */}
             <Button 
                 className="text-xl py-6 px-8"
                 onClick={() => setMatchState(4)}
