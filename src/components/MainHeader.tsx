d83bc41e604afccabe01f9e370460922b6254e26
sphealmeon <haolunqiu@gmail.com>
2025-02-15 13:27:45 -0500

    header styling

--- src/components/MainHeader.tsx
+++ src/components/MainHeader.tsx
@@ -29,7 +29,7 @@ export function MainHeader() {
     <a
       key={link.label}
       href={link.link}
-      className="px-3 py-2 hover:bg-green-200 text-lg flex items-center gap-2 rounded"
+      className="px-3 py-2 hover:bg-gray-200 hover:text-black text-lg flex items-center gap-2 rounded text-white transition-colors"
     >
       {link.label === "Scout" && (
         <FontAwesomeIcon icon={faHouse} className="w-4 h-4" />
