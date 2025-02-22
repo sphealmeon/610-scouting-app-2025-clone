0d9d31a8c0b90ccec2269f0dedc130dbf5fd3766
sphealmeon <haolunqiu@gmail.com>
2025-02-21 19:26:56 -0500

    Revert "working firebase now"
    This reverts commit ce4b0b186baf204bfd2aa17ab18e0624e8893bb2.

--- src/app/firebase/submitHP.ts
+++ src/app/firebase/submitHP.ts
@@ -12,42 +12,34 @@ export const SubmitHP = async ({
   matchData: HumanPlayerData;
 }) => {
   try {
-    console.log("Starting submission with data:", { team1, team2, matchData });
+    // Debug logs
+    console.log("Submitting data:", { team1, team2, matchData });
 
     // Update team1 (red)
-    const team1Ref = doc(db, "humanplayers", team1.toString());
+    const team1Ref = doc(db, team1.toString(), "humanplayer");
     const team1Doc = await getDoc(team1Ref);
-    const team1Data = team1Doc.data() || { 
-      totalScored: 0, 
-      totalMissed: 0,
-      matchesPlayed: 0,
-      fieldGoalPercentage: 0,
-      pointsPerGame: 0
-    };
+    const team1Data = team1Doc.data();
     
-    // Calculate new stats for team1
-    const redTotal = (team1Data.totalScored || 0) + matchData.red.redScored;
-    const redMissed = (team1Data.totalMissed || 0) + matchData.red.redMissed;
-    const redMatches = (team1Data.matchesPlayed || 0) + 1;
-    const redFGPercentage = (redTotal + redMissed) > 0 ? 
-      Math.round((redTotal / (redTotal + redMissed)) * 1000) / 10 : 0;
-    const redPPG = redMatches > 0 ? 
-      Math.round((redTotal * 4 / redMatches) * 10) / 10 : 0;
+    console.log("Existing team1 data:", team1Data);
+    console.log("New red data:", matchData.red);
 
-    const team1NewData = {
+    const redTotal = (team1Data?.totalScored || 0) + matchData.red.redScored;
+    const redMissed = (team1Data?.totalMissed || 0) + matchData.red.redMissed;
+    const redAccuracy = (redTotal + redMissed) > 0 ? 
+      Math.round((redTotal / (redTotal + redMissed)) * 100) : 0;
+
+    console.log("Calculated red values:", { redTotal, redMissed, redAccuracy });
+
+    await setDoc(team1Ref, {
+      accuracy: redAccuracy,
       team: team1,
       totalScored: redTotal,
       totalMissed: redMissed,
-      matchesPlayed: redMatches,
-      fieldGoalPercentage: redFGPercentage,
-      pointsPerGame: redPPG
-    };
-
-    console.log("Saving team 1 data:", team1NewData);
-    await setDoc(team1Ref, team1NewData, { merge: true });
+      totalScored: redTotal
+    });
 
     // Update team2 (blue)
-    const team2Ref = doc(db, "humanplayers", team2.toString());
+    const team2Ref = doc(db, team2.toString(), "humanplayer");
     const team2Doc = await getDoc(team2Ref);
     const team2Data = team2Doc.data() || { 
       totalScored: 0, 
@@ -57,31 +49,21 @@ export const SubmitHP = async ({
       pointsPerGame: 0
     };
 
-    // Calculate new stats for team2
-    const blueTotal = (team2Data.totalScored || 0) + matchData.blue.blueScored;
-    const blueMissed = (team2Data.totalMissed || 0) + matchData.blue.blueMissed;
-    const blueMatches = (team2Data.matchesPlayed || 0) + 1;
-    const blueFGPercentage = (blueTotal + blueMissed) > 0 ? 
-      Math.round((blueTotal / (blueTotal + blueMissed)) * 1000) / 10 : 0;
-    const bluePPG = blueMatches > 0 ? 
-      Math.round((blueTotal * 4 / blueMatches) * 10) / 10 : 0;
+    // Add new match data to existing totals
+    const blueTotal = team2Data.totalScored + matchData.blue.blueScored;
+    const blueMissed = team2Data.totalMissed + matchData.blue.blueMissed;
+    const blueAccuracy = (blueTotal + blueMissed) > 0 ? 
+      Math.round((blueTotal / (blueTotal + blueMissed)) * 100) : 0;
 
-    const team2NewData = {
-      team: team2,
+    await setDoc(team2Ref, {
       totalScored: blueTotal,
       totalMissed: blueMissed,
-      matchesPlayed: blueMatches,
-      fieldGoalPercentage: blueFGPercentage,
-      pointsPerGame: bluePPG
-    };
+      accuracy: blueAccuracy,
+      team: team2,
+      matchesPlayed: (team2Data.matchesPlayed || 0) + 1
+    });
 
-    console.log("Saving team 2 data:", team2NewData);
-    await setDoc(team2Ref, team2NewData, { merge: true });
-
-    console.log("Successfully saved both team data");
-    return true;
   } catch (e) {
     console.error("Error submitting HP data:", e);
-    throw e;
   }
 };
