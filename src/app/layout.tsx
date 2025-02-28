80706b3c720ef2891eb82c25e6e066bd3fc281ee
sphealmeon <haolunqiu@gmail.com>
2025-02-27 17:47:49 -0800

    new icon on the tab

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/logo.svg" type="image/svg+xml" />
+      </head>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
       </body>
