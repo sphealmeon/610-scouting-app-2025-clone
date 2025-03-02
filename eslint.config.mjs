5b9504f34c2a5d4efbe37178b33600dfd814313b
sphealmeon <haolunqiu@gmail.com>
2025-03-01 22:00:40 -0800

    rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -16,6 +16,7 @@ const eslintConfig = [
       "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/no-unsafe-function-type": "off",
       "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-unused-expressions": "off",
       "react-hooks/exhaustive-deps": "off"
     }
   }
