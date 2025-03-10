406a2ed27cd175786056467263a2945c5d5f399e
remyyan <remyyan06@crescentschool.org>
2025-03-10 12:11:13 -0400

    added the admin page through scout name

--- src/app/scout/start/scoutselect.tsx
+++ src/app/scout/start/scoutselect.tsx
@@ -1,9 +1,13 @@
+"use client"
+
 import { Button } from "@/components/ui/button";
 import { ScoutingData } from "@/app/scout/data";
 import { useState, useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 export default function ScoutSelect({ setMatchState }: { setMatchState: Function }) {
     const [isReady, setIsReady] = useState(false);
+    const router = useRouter();
 
     useEffect(() => {
         const checkReadyState = () => {
@@ -20,11 +24,22 @@ export default function ScoutSelect({ setMatchState }: { setMatchState: Function
         return () => clearInterval(interval);
     }, []);
 
+    const handleRobotScout = () => {
+        // Check for secret scout names
+        if (ScoutingData.start.scoutName === "admin") {
+            // Redirect to admin page
+            router.push("/admin");
+            return;
+        }
+        
+        setMatchState("auto");
+    };
+
     return (
         <div className="flex flex-col justify-center items-center">
             <Button
                 className={`mb-7 w-48 text-xl py-6 px-4 bg-green-900 text-white hover:bg-green-800 border-2 border-green-900`}
-                onClick={() => setMatchState(1)}
+                onClick={handleRobotScout}
                 disabled={!isReady}
             >
                 Robot Scout
