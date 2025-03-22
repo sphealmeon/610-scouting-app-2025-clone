6fd1c4f2578fa87a414a69ff7918aa7021dd2900
remyyan <remyyan06@crescentschool.org>
2025-03-22 12:51:24 -0400

    added avg fouls to stats

--- src/app/stats/all/tables/allTable.tsx
+++ src/app/stats/all/tables/allTable.tsx
@@ -40,6 +40,7 @@ export default function AllTable({ teamData }: { teamData: AggregateData[] }) {
     shallowAccuracy: (data: AggregateData) => data.shallowAccuracy,
     deepAccuracy: (data: AggregateData) => data.deepAccuracy,
     playedDefenseMatches: (data: AggregateData) => data.playedDefenseMatches,
+    avgFouls: (data: AggregateData) => data.avgFouls,
     brokePercentage: (data: AggregateData) => data.brokePercentage,
   };
 
@@ -108,6 +109,7 @@ export default function AllTable({ teamData }: { teamData: AggregateData[] }) {
               <TableCell>{(data.shallowAccuracy * 100).toFixed(1)}%</TableCell>
               <TableCell>{(data.deepAccuracy * 100).toFixed(1)}%</TableCell>
               <TableCell>{data.playedDefenseMatches}</TableCell>
+              <TableCell>{(data.avgFouls).toFixed(1)}</TableCell>
               <TableCell>{(data.brokePercentage * 100).toFixed(1)}%</TableCell>
             </TableRow>
           ))}
