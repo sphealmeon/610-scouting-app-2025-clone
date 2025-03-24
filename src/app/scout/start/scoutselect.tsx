d5c730185507e63827dc584668a6e3113b912579
Jack Zhou <jackzhou25@crescentschool.org>
2025-03-24 16:18:14 -0400

    welkdhwle

--- src/app/scout/start/scoutselect.tsx
+++ src/app/scout/start/scoutselect.tsx
@@ -6,7 +6,7 @@ export default function ScoutSelect({setMatchState}: {setMatchState: Function}){
 
     return(
         <div className="flex flex-col justify-center items-center space-y-4">
-            <Button disabled = {matchverify()}
+            <Button 
                 className="mb-20 text-xl py-6 px-8"
                 onClick={() => setMatchState(1)}
             >
